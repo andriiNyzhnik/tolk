1c83794dc5a45b1d2b23bf74aa144b7da32715c6
rkyrychuk <ruslan.kyrychuk@gmail.com>
2011-11-22 13:55:15 +0200

    Use actual table name

--- app/models/tolk/translation.rb
+++ app/models/tolk/translation.rb
@@ -2,7 +2,7 @@ module Tolk
   class Translation < ActiveRecord::Base
     set_table_name "tolk_translations"
 
-    scope :containing_text, lambda {|query| where("tolk_translations.text LIKE ?", "%#{query}%") }
+    scope :containing_text, lambda {|query| where("#{self.table_name}.text LIKE ?", "%#{query}%") }
 
     serialize :text
     validates_presence_of :text, :if => proc {|r| r.primary.blank? && !r.explicit_nil }
